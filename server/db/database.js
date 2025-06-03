8c012e52f57b841d48a44cc0a88be60d34f0c604
Tessio94 <simeklapan@gmail.com>
2025-06-03 13:25:10 +0200

    added connection to render database on backend

--- server/db/database.js
+++ server/db/database.js
@@ -3,16 +3,23 @@ const { Pool } = pg;
 
 require("dotenv").config();
 
-const password = process.env.DB_PSSW;
+// const password = process.env.DB_PSSW;
+
+// const pool = new Pool({
+// 	host: "localhost",
+// 	user: "postgres",
+// 	database: "postgres",
+// 	password: password,
+// 	max: 20,
+// 	idleTimeoutMillis: 30000,
+// 	connectionTimeoutMillis: 2000,
+// });
 
 const pool = new Pool({
-	host: "localhost",
-	user: "postgres",
-	database: "postgres",
-	password: password,
-	max: 20,
-	idleTimeoutMillis: 30000,
-	connectionTimeoutMillis: 2000,
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 module.exports = pool;
