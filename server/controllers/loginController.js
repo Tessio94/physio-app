99e78ef955f3015cd625ebae437d9de11ea79344
Tessio94 <simeklapan@gmail.com>
2025-06-09 11:04:15 +0200

    Update loginController.js

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -380,7 +380,7 @@ const adminLogin = async (req, res) => {
 		res.cookie("admin_token", token, {
 			httpOnly: true,
 			secure: process.env.NODE_ENV === "production",
-			sameSite: "lax",
+			sameSite: "none",
 			maxAge: 7 * 24 * 60 * 1000,
 		});
 
