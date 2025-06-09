e2c34f525b39d4f8143cc1c872841b33b4785d1c
Tessio94 <simeklapan@gmail.com>
2025-06-09 11:17:03 +0200

    Update AdminLayout.tsx

--- client/src/layout/AdminLayout.tsx
+++ client/src/layout/AdminLayout.tsx
@@ -5,7 +5,7 @@ import { AppSidebar } from "@/components/AppSidebar";
 import { useEffect } from "react";
 
 const fetchAdminInfo = async () => {
-  const res = await fetch("${prodUrl}/auth/admin/current", {
+  const res = await fetch(`${prodUrl}/auth/admin/current`, {
     method: "GET",
     credentials: "include",
   });
