dafaec9064a5418494540df698230572b1516ccd
Tessio94 <simeklapan@gmail.com>
2025-06-09 10:36:46 +0200

    Update AdminDashboard.tsx

--- client/src/pages/admin/AdminDashboard.tsx
+++ client/src/pages/admin/AdminDashboard.tsx
@@ -77,8 +77,8 @@ const fetchAdminDashboardData = async (adminId: number) => {
 const AdminDashboard = () => {
   const admin = useOutletContext<Admin>();
   // console.log(admin); - OVJDJE UBACITI ERROR AKO JE ADMIN UNDEFINED (DA GA VRAĆA NA LOG-IN, TO JEST VIDJETI DA LI TO MOZE ICI SA SERVERA BEZ DA DODJE DO OVE LINIJE I DA KORISITIMO SOONNER UMJESTO ALERTA.)
-  const { adminId, name, lastname, superadmin } = admin;
   console.log(admin);
+  const { adminId, name, lastname, superadmin } = admin;
   const { data, isLoading } = useQuery({
     queryKey: ["monthly-users"],
     queryFn: fetchDashboardData,
