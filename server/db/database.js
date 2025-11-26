ed5331208544b40c5f34bcca5c92b4d7f1b66b5e
Tessio94 <simeklapan@gmail.com>
2025-11-26 21:05:13 +0100

    added db string

--- server/db/database.js
+++ server/db/database.js
@@ -3,23 +3,23 @@ const { Pool } = pg;
 
 require("dotenv").config();
 
-// const password = process.env.DB_PSSW;
-
-// const pool = new Pool({
-// 	host: "localhost",
-// 	user: "postgres",
-// 	database: "postgres",
-// 	password: password,
-// 	max: 20,
-// 	idleTimeoutMillis: 30000,
-// 	connectionTimeoutMillis: 2000,
-// });
+const password = process.env.DB_PSSW;
 
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: false,
-	},
+  host: "localhost",
+  user: "postgres",
+  database: "postgres",
+  password: password,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
+
+// const pool = new Pool({
+// 	connectionString: process.env.DATABASE_URL,
+// 	ssl: {
+// 		rejectUnauthorized: false,
+// 	},
+// });
 
 module.exports = pool;
