7ae8af961d59fcae33f62e128a8eeada52f07e98
Tessio94 <simeklapan@gmail.com>
2025-11-27 12:10:48 +0100

    fixed cors

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 ///////////////////////modules////////////////////////////////
 const corsOptions = {
 	// origin: "http://localhost:5173", // Frontend URL (React app)
-	origin: "https://api.app2.tessio94.com/",
+	origin: "https://app2.tessio94.com",
 	methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
 	allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
 	credentials: true, // Allow cookies to be sent
